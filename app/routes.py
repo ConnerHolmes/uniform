001b62a706f8fb179963954296255d5fc1262c5c
DrKinbote <conner.a.holmes@gmail.com>
2025-03-24 14:59:49 -0500

    Updates
    Completed tutorial part 11 (added Bootstrap)

--- app/routes.py
+++ app/routes.py
@@ -7,8 +7,11 @@ from app.forms import RegistrationForm
 from app.forms import EditProfileForm
 from app.forms import EmptyForm
 from app.forms import PostForm
+from app.forms import ResetPasswordRequestForm
+from app.forms import ResetPasswordForm
 from app.models import User
 from app.models import Post
+from app.email import send_password_reset_email
 from flask_login import current_user, login_user
 from flask_login import logout_user
 from flask_login import login_required
@@ -188,3 +191,38 @@ def explore():
 
     return render_template('index.html', title='Explore', posts=posts.items,
                            next_url=next_url, prev_url=prev_url)
+
+@app.route('/reset_password_request', methods=['GET','POST'])
+def reset_password_request():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+
+    form = ResetPasswordRequestForm()
+
+    if form.validate_on_submit():
+        user = db.session.scalar(
+                sa.select(User).where(User.email == form.email.data))
+        if user:
+            send_password_reset_email(user)
+        flash('Reset instructions have been dispatched')
+        return redirect(url_for('login'))
+    return render_template('reset_password_request.html',
+                           title='Reset Password', form=form)
+
+@app.route('/reset_password/<token>', methods=['GET','POST'])
+def reset_password(token):
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    
+    user = User.verify_reset_password_token(token)
+    
+    if not user:
+        return redirect(url_for('index'))
+
+    form = ResetPasswordForm()
+    if form.validate_on_submit():
+        user.set_password(form.password.data)
+        db.session.commit()
+        flash('Password reset complete')
+        return redirect(url_for('login'))
+    return render_template('reset_password.html', form=form)
